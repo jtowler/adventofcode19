015782c24aa32807d0846abfbbe69e38e09b31c4
themother <jacktowleretc@hotmail.com>
2020-12-11 09:43:27 +0000

    rolled back changes to a working version, day 11 2020 done

--- src/advent20/day11.py
+++ src/advent20/day11.py
@@ -5,15 +5,16 @@ from functools import partial
 def get_seat(old_layout: List[str], y: int, x: int, yi: int, xi: int) -> str:
     new_y = y + yi
     new_x = x + xi
-    if (0 <= new_y < Y_LEN) and (0 <= new_x < X_LEN):
+    if (0 <= new_y < y_len) and (0 <= new_x < x_len):
         return old_layout[new_y][new_x]
-    return '.'
+    else:
+        return '.'
 
 
 def get_seat_in_dir(old_layout: List[str], y: int, x: int, y_dir: int, x_dir: int) -> str:
     new_y = y + y_dir
     new_x = x + x_dir
-    while (0 <= new_y < Y_LEN) & (0 <= new_x < X_LEN):
+    while (0 <= new_y < y_len) & (0 <= new_x < x_len):
         seat = old_layout[new_y][new_x]
         if seat != '.':
             return seat
@@ -62,14 +63,15 @@ def part(old_layout: List[str], func) -> int:
         new_layout = get_new_layout(old_layout, func)
         if new_layout == old_layout:
             return sum(map(lambda x: x.count('#'), old_layout))
-        old_layout = new_layout
+        else:
+            old_layout = new_layout
 
 
 if __name__ == '__main__':
-    LAYOUT = open("../../resources/advent20/input11.txt", "r").read().split("\n")
-    Y_LEN = len(LAYOUT)
-    X_LEN = len(LAYOUT[0])
-    ANSWER1 = part(LAYOUT, should_change1)
-    ANSWER2 = part(LAYOUT, should_change2)
-    print(ANSWER1)  # 2303
-    print(ANSWER2)  # 2057
+    layout = open("../../resources/advent20/input11.txt", "r").read().split("\n")
+    y_len = len(layout)
+    x_len = len(layout[0])
+    answer1 = part(layout, should_change1)
+    answer2 = part(layout, should_change2)
+    print(answer1)  # 2303
+    print(answer2)  # 2057
